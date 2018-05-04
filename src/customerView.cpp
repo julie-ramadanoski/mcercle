d925b1f56d8d8765e0a250bce7820ea456ed0a13
julie-ramadanoski <ramadanoski.j@gmail.com>
2018-05-04 08:49:52 +0200

    supprime elements inutiles

--- src/customerView.cpp
+++ src/customerView.cpp
@@ -1,20 +1,16 @@
 /**
   This file is a part of mcercle
   Copyright (C) 2010-2013 Cyril FRAUSTI
-
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
-
   You should have received a copy of the GNU General Public License
   along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 #include "customerView.h"
@@ -402,7 +398,6 @@ void customerView::on_tableWidget_itemSelectionChanged(){
 }
 
 /**
-
   */
 void customerView::loadInfoCustomer(){
 	//Si on est pas connecte on sort
@@ -749,7 +744,6 @@ void customerView::listInvoicesToTable(QString filter, QString field)
 		ItemOfTable *item_PRICE        = new ItemOfTable();
 		ItemOfTable *item_PRICE_TAX    = new ItemOfTable();
 		ItemOfTable *item_PARTPAYMENT  = new ItemOfTable();
-		ItemOfTable *item_PAYMENTDATE  = new ItemOfTable();
 		ItemOfTable *item_DESCRIPTION  = new ItemOfTable();
 		ItemOfTable *item_STATE        = new ItemOfTable();
 
@@ -763,7 +757,6 @@ void customerView::listInvoicesToTable(QString filter, QString field)
 		}
 		else {
 			item_PARTPAYMENT->setData(Qt::DisplayRole, ilist.part_payment.at(i));
-			item_PAYMENTDATE->setData(Qt::DisplayRole, ilist.paymentdate.at(i));
 		}
 		item_DESCRIPTION->setData(Qt::DisplayRole, ilist.description.at(i));
 		item_STATE->setIcon( m_data->m_customer->m_invoice->getIconState(ilist.state.at(i)) );
@@ -781,7 +774,6 @@ void customerView::listInvoicesToTable(QString filter, QString field)
 		ui->tableWidget_Invoices->setItem(i, 5, item_PARTPAYMENT);
 		ui->tableWidget_Invoices->setItem(i, 6, item_DESCRIPTION);
 		ui->tableWidget_Invoices->setItem(i, 7, item_STATE);
-		ui->tableWidget_Invoices->setItem(i, 8, item_PAYMENTDATE);
 	}
 	ui->tableWidget_Invoices->setSortingEnabled(true);
 	ui->tableWidget_Invoices->selectRow(0);
@@ -1382,5 +1374,3 @@ void customerView::pasteInvoice(){
 	}
 
 }
-
-
